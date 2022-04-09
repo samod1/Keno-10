63ed7de3de48e4d4888c8fdf0ee2c7309f4e2f65
Samuel Domin <samuel.domin@dominanz.sk>
2022-04-09 17:39:26 +0200

    Skoro spravny program, este dokoncit generovanie cisiel

--- Program.cs
+++ Program.cs
@@ -5,23 +5,26 @@ namespace Lucka_Keno
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             int[] tipovane = new int[10];
-            
+            int[] losovane = new int[10];
 
-        Console.WriteLine("\nKeno 10\n");
-        Console.WriteLine("Vloz 10 cisel");
 
+            Console.WriteLine("\nKeno 10\n");
+            Console.WriteLine("Vloz 10 cisel");
+
+            //zapis do tipovanych cisiel do pola
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("\nNapis cislo "+ i);
+                Console.WriteLine("\nNapis cislo " + i);
                 tipovane[i] = Convert.ToInt32(Console.ReadLine());
 
                 if (Enumerable.Range(1, 80).Contains(tipovane[i]))
                 {
 
-                }    
+                }
 
                 else
                 {
@@ -31,21 +34,63 @@ namespace Lucka_Keno
                 }
             };
 
+            Console.WriteLine("Vase Tipy");
             VypisTipy(tipovane);
+
+            for (int i = 0; i < 10; i++)
+            {
+                losovane[i] = GenerujLosovanie();
+            }
+
+
+            Console.WriteLine("Vylosovane cisla");
+            VypisTipy(losovane);
+
+            Console.WriteLine("Spravne si tipol: "+ VyhodnotenieLosovania(losovane,tipovane));
 
         }
 
         //funkcia na vypis pola
-        public static void VypisTipy(int[] losovanie)
+        public static void VypisTipy(int[] cisla)
         {
-            Console.WriteLine("\n\nZoradene prvky pola");
+            
 
-            Array.Sort(losovanie);
-            foreach (int prvok in losovanie)
+            Array.Sort(cisla);
+            foreach (int prvok in cisla)
             {
                 Console.WriteLine(prvok + " ");
-            }    
+            }
+        }
+    
+
+        static int GenerujLosovanie()
+        {
+            Random rd = new Random();
+            int rand = rd.Next(1,80);
+
+            return rand;
+        }
+
+        static int VyhodnotenieLosovania(int[] losovane, int[]tipovane)
+        {
+            int pocet_spravnych_tipov = 0;
+
+            for(int i=0; i < 10; i++)
+            {
+                int index = Array.IndexOf(losovane, tipovane[i]);
+                    if(index == -1)
+                    {
+                        //nebolo najdene cislo nepotrebujeme ziaden vypis
+                    }
+
+                    else
+                    {
+                    pocet_spravnych_tipov += 1;
+                    }
+            }
+            return pocet_spravnych_tipov;
         }
     }
-    
 }
+    
+
